44fa009d48e16a37e6c9cbea07f5b3da8c8014f1
yg2348 <yg2348@nyu.edu>
2024-12-12 12:47:49 -0500

    deployment

--- api/server.js
+++ api/server.js
@@ -10,9 +10,7 @@ port=3000
 app.set("view engine", "ejs");
 
 // Set the directory for EJS templates
-app.set("views", path.join(__dirname, "views")); // Ensure 'views' folder exists
-
-// Serve static files (CSS, images, JS)
+app.set("views", path.join(__dirname, "views")); 
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
@@ -21,6 +19,7 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Respiro",
   });
+  res.send("Express on Vercel")
 });
 
 app.get("/box", (req, res) => {
